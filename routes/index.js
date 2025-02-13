98465c7cc404e696c8afe9421288d2b6c616c901
biurraai <auria@ayesa.com>
2025-02-13 17:11:05 +0100

    Cambio minimo de titulo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var title = 'Express';
+var appName = 'AppChat';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title, appName });
 });
 
 module.exports = router;
